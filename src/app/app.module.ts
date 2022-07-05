42f0308be9bc16ad09df1597a15c508adac76045
ryuzaki944 <1l-94@mail.ru>
2022-07-06 01:02:24 +0600

    v 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DropZoneComponent } from './drop-zone/drop-zone.component';
+import { DndDirective } from './dnd.directive';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropZoneComponent,
+    DndDirective,
+    ProgressComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dnd.directive.spec.ts
@@ -0,0 +1,9 @@
+import { DndDirective } from './dnd.directive';
+
+describe('DndDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DndDirective();
+    expect(directive).toBeTruthy();
+  });
+  
+});
